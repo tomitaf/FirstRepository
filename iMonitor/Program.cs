cc6b1840604c951a3d7ab142fdc7f49e445f3e1e
FMT <felipe.tomita@gmail.com>
2020-05-17 14:18:09 -0300

    Versao 1.0 - Display de mensagens

---
+++ iMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
